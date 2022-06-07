491ecedde179bd6cbc7cb294f115a76209698dae
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-06-06 22:39:57 -0600

    updated emp file

--- server.js
+++ server.js
@@ -1,51 +1,43 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { viewAllEmp, addEmp, upEmp } = require('./lib/employee');
+const { viewDep, addDep } = require('./lib/dept');
+const { viewRoles, addRole } = require('./lib/role');
 
-const questions = () => {
+questions = () => {
     inquirer
-
         .prompt({
-
-            type: 'list',
-            name: 'choices',
-            message: 'What would you like to do?',
-            choices: ['View Depts', 'View rols', 'View employees', 'Add dept', 'Add employee', 'Add role', 'Update employee role', 'I am finished']
-
-        }).then((data) => {
-            console.log(data.choices);
-            switch (data.choices) {
-                case 'View Departments':
-                    viewDep();
-                    break;
-                case 'View Roles':
-                    viewRoles();
-                    break;
-                case 'View Employees':
-                    viewAllEmp();
-                    break;
-                case 'Add Department':
-                    addDep();
-                    break;
-                case 'Add Employee':
-                    addEmp();
-                    break;
-                case 'Add Role':
-                    addRole();
-                    break;
-                case 'Update Employee Role':
-                    upEmp();
-                    break;
-                case 'I am finished':
-                    break;
-            }
-        })
+        type: 'list',
+        name: 'choices',
+        message: 'What would you like to do?',
+        choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Employee', 'Add Role', 'Update Employee Role', 'I am Finished']
+    }).then((data) => {
+        switch (data.choices) {
+            case 'View Departments':
+                viewDep();
+                break;
+            case 'View Roles':
+                viewRoles();
+                break;
+            case 'View Employees':
+                viewAllEmp();
+                break;
+            case 'Add Department':
+                addDep();
+                break;
+            case 'Add Employee':
+                addEmp();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Update Employee Role':
+                upEmp();
+                break;
+            case 'I am Finished':
+                break;
+        }
+        console.log(data.choices + ' chosen!!!');
+    })
 };
 questions();
-
-
-
-
-module.exports = {questions};
-const { viewAllEmp, addEmp, upEmp } = require('./lib/employee.js');
-const { viewDep, addDep } = require('./lib/dept.js');
-const { viewRoles, addRole } = require('./lib/role.js');
